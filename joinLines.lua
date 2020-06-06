50d038ce2e462de58aee076332dac9969a2d817b
Patrick Weingärtner <patrick.weingaertner91@gmail.com>
2020-06-06 11:50:57 +0200

    drop support for micro <2.0.4. fix bug that unicode characters are counted wrong resulting in deleting a line.

--- joinLines.lua
+++ joinLines.lua
@@ -1,7 +1,11 @@
-VERSION = "1.0.3"
+VERSION = "1.1.4"
 
-function joinLines()
-    local v = CurView()
+local micro = import("micro")
+local config = import("micro/config")
+local buffer = import("micro/buffer")
+local util = import("micro/util")
+
+function joinLines(v)
     local a, b, c = nil, nil, v.Cursor
     local selection = c:GetSelection()
 
@@ -11,36 +15,38 @@ function joinLines()
         else
             a, b = c.CurSelection[1], c.CurSelection[2]
         end
-        a = Loc(a.X, a.Y)
-        b = Loc(b.X, b.Y)
+        a = buffer.Loc(a.X, a.Y)
+        b = buffer.Loc(b.X, b.Y)a
         selection = c:GetSelection()
     else
         -- get beginning of curent line
-        local startLoc = Loc(0, c.Loc.Y)  
+        local startLoc = buffer.Loc(0, c.Loc.Y)  
         -- get the last position of the next line 
         -- I use the go function because Lua string.len counts bytes which leads
         -- to wrong results with some unicode characters
-        local xNext = utf8.RuneCountInString(v.Buf:Line(c.Loc.Y+1))
+        local xNext = util.CharacterCountInString(v.Buf:Line(c.Loc.Y+1))
 
         a = startLoc
-        b = Loc(xNext, c.Loc.Y+1)
+        b = buffer.Loc(xNext, c.Loc.Y+1)
+        if a.x ~= b.x then
+            c:SetSelectionStart(startLoc)
+            c:SetSelectionEnd(b)
+            selection = c:GetSelection()
+        end
+    end
 
-		if a.x ~= b.x then 
-        	c:SetSelectionStart(startLoc)
-        	c:SetSelectionEnd(b)
-        	selection = c:GetSelection()
-       	end
-    end    
+    if a.x ~= b.x then
+        selection = util.String(selection)
 
-	if a.x ~= b.x then 
-    	-- swap all whitespaces with a single space
-    	local modifiedSelection = string.gsub(selection, "\n%s*", " ")
-    	-- write modified selection to buffer
-    	v.Buf:Replace(a, b, modifiedSelection)    
-   	end
+        -- swap all whitespaces with a single space
+        local modifiedSelection = string.gsub(selection, "\n%s+", " ")
+        -- write modified selection to buffer
+        v.Buf:Replace(a, b, modifiedSelection)
+    end
 end
 
-MakeCommand("joinLines", "joinLines.joinLines")
-BindKey("Alt-j", "joinLines.joinLines")
-
-AddRuntimeFile("joinLines", "help", "help/join-lines-plugin.md")
+function init()
+    config.MakeCommand("joinLines", joinLines, config.NoComplete)
+    config.TryBindKey("Alt-j", "lua:joinLines.joinLines", false)
+    config.AddRuntimeFile("joinLines", config.RTHelp, "help/join-lines-plugin.md")
+end
